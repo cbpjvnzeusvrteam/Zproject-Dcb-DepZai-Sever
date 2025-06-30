a6e32f0240d5e48f46d48059ec7904c8dcae0d06
cbpjvnzeusvrteam <duongcongbang.dev@gmail.com>
2025-06-30 08:51:03 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dataall_handler import handle_dataall
 from greeting_group_handler import handle_bot_added
 from help_handler import handle_help
 from tts_handler import handle_tts
-from config_handler import handle_config
+from config_module import handle_config
 from about_handler import handle_about
 from callback import handle_retry_button, handle_tts_button
 from noti_handler import handle_noti
