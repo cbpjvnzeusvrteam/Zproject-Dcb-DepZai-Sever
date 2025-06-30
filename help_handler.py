71aaa1e0016cabf4c8527206a77dd987573e6441
cbpjvnzeusvrteam <duongcongbang.dev@gmail.com>
2025-06-30 09:01:28 +0700

    Add files via upload

--- help_handler.py
+++ help_handler.py
@@ -1,9 +1,11 @@
 def handle_help(bot, message):
     reply = (
         "<b>📖 Lệnh hỗ trợ:</b>\n\n"
+        "<code>/start</code> - Bắt Đầu\n"
         "<code>/ask</code> – Hỏi AI\n"
         "<code>/tts văn bản</code> – Chuyển văn bản thành giọng nói\n"
         "<code>/about</code> – Giới thiệu về bot\n"
+        "<code>/noti</code> - Thông Báo All Sever ( Admin )\n"
         "<code>/dataall</code> – Thống kê người dùng ( Admin )\n"
         "<code>/time</code> – Thời gian uptime"
     )
